fe707b1664b3c2f841b539dcb425573216cdb93c
Jeff <jeffersonkadams@gmail.com>
2018-07-13 09:12:14 -0400

    finished functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 console.log("loaded!");
 
-console.log(feet, inches, weight);
-
 function calculateBMI(){
-  var feet = document.getElementById("feet").value;
-  var inches = document.getElementById("inches").value;
-  var weight = document.getElementById("pounds").value;
+  // the + in front of document forces the returned .value to be a numbwe instead of a string (heightInInches was concatenating feet*12 and inches.)
+  var feet = +document.getElementById("feet").value;
+  var inches = +document.getElementById("inches").value;
+  var weight = +document.getElementById("pounds").value;
   var heightInInches = (feet * 12) + inches;
-  console.log(heightInInches, weight);
+  console.log(weight, feet, inches, heightInInches);
+
+  //Formula: weight (lb) / [height (in)]^2 x 703
+  var bmi = (weight / heightInInches / heightInInches) * 703;
+  document.getElementById("output").innerHTML = bmi;
+  console.log(bmi);
 }
